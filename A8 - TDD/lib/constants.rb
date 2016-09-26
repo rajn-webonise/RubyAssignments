fdcdd72d9b3c29a71774cde30e8e41062bc84eca
Raj Negi <raj.negi@weboniselab.com>
2016-09-26 18:05:09 +0530

    Complete basic functionality

---
+++ A8 - TDD/lib/constants.rb
@@ -0,0 +1,18 @@
+
+$VALUE = Hash.new
+$VALUE[:I] = 1
+$VALUE[:V] = 5
+$VALUE[:X] = 10
+$VALUE[:L] = 50
+$VALUE[:C] = 100
+$VALUE[:D] = 500
+$VALUE[:M] = 1000
+#
+# $VALUE = Hash.new
+# $VALUE[:I] = {value: 1, label: "coin"}
+# $VALUE[:V] = {value: 5, label: "dollar"}
+# $VALUE[:X] = {value: 10, label: "yen"}
+# $VALUE[:L] = {value: 50, label: "rupee"}
+# $VALUE[:C] = {value: 1, label: "c"}
+# $VALUE[:D] = {value: 1, label: "d"}
+# $VALUE[:M] = {value: 1, label: "m"}
